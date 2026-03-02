9ca46adcf8fe5628c2dffeea3fc926f945df7261
Emirhan <106195130+EmirhanS@users.noreply.github.com>
2026-03-02 11:56:16 +0300

    fix

--- tracker/migrations/0003_alter_activity_activity_type_and_more.py
+++ tracker/migrations/0003_alter_activity_activity_type_and_more.py
@@ -38,6 +38,7 @@ class Migration(migrations.Migration):
                 choices=[
                     ("training", "Antrenmana Katılım"),
                     ("analysis", "Analize Katılım"),
+                    ("tournament", "Turnuva Katılımı"),
                     ("match_watching", "Maç İzleme"),
                     ("other_team_frisbee", "Farklı Takım Frizbi Antrenmanı"),
                     ("upper_body", "Üst Vücut Antrenmanı"),
@@ -46,7 +47,6 @@ class Migration(migrations.Migration):
                     ("other_sport", "Farklı Spor"),
                     ("flexibility", "Yoga / Pilates / Mobilite"),
                     ("hf_disk", "HF Disk"),
-                    ("tournament", "Turnuva Katılımı"),
                 ],
                 max_length=50,
             ),
